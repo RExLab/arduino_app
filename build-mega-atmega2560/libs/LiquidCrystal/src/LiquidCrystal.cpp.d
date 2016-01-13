1e80ba96e37180742431ac3d0c4d564c0b2720af
joao <joao@joao-K46CA>
2016-01-13 15:35:06 -0300

    first commit

---
+++ build-mega-atmega2560/libs/LiquidCrystal/src/LiquidCrystal.cpp.d
@@ -0,0 +1,15 @@
+build-mega-atmega2560/libs/LiquidCrystal/src/LiquidCrystal.cpp.o: \
+ /usr/share/arduino/libraries/LiquidCrystal/src/LiquidCrystal.cpp \
+ /usr/share/arduino/libraries/LiquidCrystal/src/LiquidCrystal.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/variants/mega/pins_arduino.h
